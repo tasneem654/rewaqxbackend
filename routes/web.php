9eff69142bdf522e3561ea198d8da2075619f9b9
Lojaen3 <lojaenayash@gmail.com>
2025-03-28 15:18:19 +0300

    empManagment route

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,16 @@ Route::get('/', function () {
 Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
 Route::post('/admin/login', [AuthController::class, 'login'])->name('admin.login.submit');
 
+
 // Admin dashboard route (protected by authentication)
 Route::get('/admin/dashboard', function () {
     return view('admin.dashboard');
 })->name('admin.dashboard')->middleware('auth');
+
+Route::get('/admin/empManagement', function () {
+    return view('admin.empManagement');
+});
+
 
 // Add additional routes for your application as needed, like:
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
